c52c919ec7a3dd522aac14ab3b3893201184e79e
Nicole S <42750985+NicoleS5@users.noreply.github.com>
2020-02-12 11:43:57 -0500

    Updates

---
+++ precache-manifest.c8296155304a84eed9498bffe840ce8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e52b9171a72fb90e32380dbf99a924",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "6e4d288f75dd926cf4f3",
+    "url": "/dig4639-mobile-dev/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4d288f75dd926cf4f3",
+    "url": "/dig4639-mobile-dev/static/js/main.1259fb09.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
