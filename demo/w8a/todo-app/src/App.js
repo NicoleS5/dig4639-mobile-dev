5c41599e5fe62b84fcc65da1ea5a66ecf599d1d2
Nicole S <42750985+NicoleS5@users.noreply.github.com>
2020-02-26 12:21:45 -0500

    #98

--- demo/w8a/todo-app/src/App.js
+++ demo/w8a/todo-app/src/App.js
@@ -1,41 +1,73 @@
 import React from 'react';
 import './App.css';
+import todoList from './todoList.json';
 
-const todoList = [
-  {
-    content: 'Task 1', priority: 2, completed: true
-  },
-  {
-    content: 'Task 2', priority: 1, completed: true
-  },
-  {
-    content: 'Task 3', priority: 3, completed: false
-  }
-]
+// Filter the list based on checkbox
+// Add an Input Form to allow creating todo items with content + priority
+// Have each item be able to remove itself using a function passed in from the parent
 
 function TodoItem(props) {
-  return <p>{props.content}</p>
+  return <p className='card' onClick={() => props.removeTask(props.id)}>{props.content}</p>
 }
 
-function App() {
-  /*
-  const todoListFiltered = todoList.filter((value) => value.completed)
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todoList,
+      hide: false,
+    }
+    this.currentId = 4;
+  }
 
-  let todoArray = todoListFiltered.map(
-    (value) => <TodoItem content={value.content}/>
-  ) 
+  addTask(e) {
+    console.log(this.refs.taskContent)
+    let todoList = this.state.todoList
+    todoList.push(
+      {"id": this.currentId, "content": this.refs.taskContent.value, "priority": 1, "completed": true}
+    )
+    this.currentId++
+    this.setState({todoList})
+  }
 
-  [
-    <TodoItem content="Item 1"/>,
-    <TodoItem content="Item 2"/>,
-    <TodoItem content="Item 3"/>
-  ]
-  */
+  removeTask(id) {
+    console.log(id)
+    let todoList = this.state.todoList
+    console.log(todoList)
+    todoList = todoList.filter((v) => v.id !== id)
+    console.log(todoList)
+    this.setState({todoList})
+  }
 
+  render() {
+    return (
+      <React.Fragment>
+        <input type="text" ref="taskContent" />
+        <input type="button" value="Add Task" onClick={(e) => this.addTask(e)}/>
+        <input ref="hideCheckbox" type="checkbox" name="hide" id="hide" value="hide" 
+          onChange={(e) => this.setState({hide: e.target.checked})}/>
+        <label htmlFor="hide"> Poooop</label><br></br>
+  
+        { ((this.state.hide) ? this.state.todoList
+          .filter((v) => !v.completed) : this.state.todoList)
+          .map ((v) => <TodoItem 
+            id = {v.id}
+            removeTask ={(id) => this.removeTask(id)}
+            priority={v.priority} 
+            key = {v.id}
+            content={v.content} 
+            completed={v.completed} 
+            />) 
+        }
+      </React.Fragment>
+    )
+  } 
+}
+
+function App(props) {
   return (
-    todoList.filter((v) => v.completed).map (
-      (v) => <TodoItem priority={v.priority} content={v.content} completed={v.completed} />)
-  );
+    <TodoList />
+  )
 }
 
 export default App;
