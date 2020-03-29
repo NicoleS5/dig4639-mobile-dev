6c0914df26d5a458a787fc2d506f88d6d70eea84
Nicole S <42750985+NicoleS5@users.noreply.github.com>
2020-03-29 12:45:11 -0400

    Lab5 Completed

---
+++ labs/5/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WeatherList from './components/WeatherList'
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+         <WeatherList />
+      </div>
+    );
+  }
+}
+
+export default App;
